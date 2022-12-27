d8797bc3f7de6ed6664360a4eb2f79ac0e6194be
Goodl3oyZ <panudetzx2@gmail.com>
2022-12-27 16:18:35 +0700

    10.2 in(;;)for is decribe everything

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
-
-//Write the function printO() here
+void printO(int a,int b)
+{
+	if(a<=0 ||b<=0)
+	{
+		cout<<"Invalid input";
+	}
+	for(int i=0;i<a;i++){
+		for(int j=0;j<b; j++){
+			cout<<"O";
+		}
+		cout<<"\n";
+	}
+}
 
 int main(){
 	
